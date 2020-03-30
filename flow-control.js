f4478cb954f37a10e673e4dca84b757703a01dd5
Miao Nan Lin <miaonanlin@gmail.com>
2020-03-30 01:21:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,28 +1,28 @@
 function basicTeenager(age) {
   if (age>=13 && age<=19){
-    return "You are a teenager!"}
+    return "You are a teenager!";}
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-return "You are a teenager!"
-} else {return "You are not a teenager"}
+return "You are a teenager!";
+} else {return "You are not a teenager";}
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-  return "You are a teenager!"}
+  return "You are a teenager!";}
   else if (age <= 12) {
-  return "You are a kid" }
+  return "You are a kid"; }
   else (age >= 20) {
-  return "You are a grownup"}
+  return "You are a grownup";}
 }
 
 
 function ternaryTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager"}
-  return "You are not a teenager"
+    return "You are a teenager";}
+  return "You are not a teenager";
 }
 
 function switchAge(age) {
